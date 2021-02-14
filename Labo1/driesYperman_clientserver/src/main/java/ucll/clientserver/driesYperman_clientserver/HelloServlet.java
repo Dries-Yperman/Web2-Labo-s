8c2736ec786ad5416516148c9b162c183fac987f
Dries-Yperman <dries.yperman@student.ucll.be>
2021-02-14 18:28:29 +0100

    2 + 2

--- Labo1/driesYperman_clientserver/src/main/java/ucll/clientserver/driesYperman_clientserver/HelloServlet.java
+++ Labo1/driesYperman_clientserver/src/main/java/ucll/clientserver/driesYperman_clientserver/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "2 + 2 = 5";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
